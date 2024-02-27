08a7d5b69e515ca426ea6dd9c86dba34fbb6e1e6
Keith Stanley <iamstan13y@gmail.com>
2024-02-27 04:04:12 +0200

    FigmaSharpX Changes.

---
+++ tested/Resources/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo ("FigmaSharpX.Cocoa")]
+[assembly: InternalsVisibleTo("FigmaSharpX.Wpf")]
+[assembly: InternalsVisibleTo("FigmaSharpX.iOS")]
+[assembly: InternalsVisibleTo("FigmaSharpX.WinForms")]
+[assembly: InternalsVisibleTo("FigmaSharpX.Android")]
