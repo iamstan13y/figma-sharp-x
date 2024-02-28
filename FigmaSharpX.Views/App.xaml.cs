8032b4b362e965b009c0ac51e5cbfa3c535e107a
Keith Stanley <iamstan13y@gmail.com>
2024-02-28 10:10:57 +0200

    Whole lot of bullstuff.

---
+++ FigmaSharpX.Views/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FigmaSharpX.Views
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FigmaSharpX.Views/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FigmaSharpX.Views
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
