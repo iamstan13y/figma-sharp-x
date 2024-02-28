9b8a76e5032bdd0f5f54ad1fd4d69e0b78c45f58
Keith Stanley <iamstan13y@gmail.com>
2024-02-28 10:09:43 +0200

    Whole lot of bullstuff.

---
+++ FigmaSharpX.Maui.Graphics/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FigmaSharpX.Maui.Graphics
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
