048d5c370a51470f6be4ee3a85d27a6ed6444175
Keith Stanley <iamstan13y@gmail.com>
2024-02-28 10:09:52 +0200

    Whole lot of bullstuff.

---
+++ FigmaSharpX.Maui.Graphics/PropertyConfigure/CodePropertyConfigure.cs
@@ -0,0 +1,14 @@
+﻿using FigmaSharpX.Converters;
+using FigmaSharpX.PropertyConfigure;
+using FigmaSharpX.Services;
+
+namespace FigmaSharpX.Maui.Graphics.PropertyConfigure
+{
+    public class CodePropertyConfigure : CodePropertyConfigureBase
+    {
+        public override string ConvertToCode(string propertyName, CodeNode currentNode, CodeNode parentNode, NodeConverter converter, CodeRenderService rendererService)
+        {
+            return string.Empty;
+        }
+    }
+}
